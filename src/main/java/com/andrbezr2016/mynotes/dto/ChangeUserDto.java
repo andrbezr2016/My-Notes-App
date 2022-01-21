7a16f7e22113b8ba045838a43b8323960f794f20
Bezrukov Andrey <andrbezr2016@yandex.ru>
2022-01-21 12:27:54 +0300

    added REST API table and ChangeUserDto

---
+++ src/main/java/com/andrbezr2016/mynotes/dto/ChangeUserDto.java
@@ -0,0 +1,13 @@
+package com.andrbezr2016.mynotes.dto;
+
+import lombok.Data;
+
+@Data
+public class ChangeUserDto {
+
+    private String username;
+
+    private String password;
+
+    private String iconPath;
+}
