771e660ba81162a59b00fe9bcf6aba167cae976b
Bezrukov Andrey <andrbezr2016@yandex.ru>
2022-01-24 08:57:57 +0300

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,12 +7,18 @@ VALUES ('1111', '1111', 1),
        ('2222', '2222', 2);
 
 INSERT INTO categories (user_id, title)
-VALUES (1, 'User1 Category'),
-       (2, 'User2 Category');
+VALUES (1, 'Category 1'),
+       (1, 'Category 2'),
+       (2, 'Category 1'),
+       (2, 'Category 2');
 
 INSERT INTO notes (category_id, title, content)
-VALUES (1, 'Note 1', 'Hello User1!'),
-       (2, 'Note 2', 'Hello User2!');
+VALUES (1, 'Note 1', 'Hello User1, Category 1!'),
+       (1, 'Note 2', 'Hello User1, Category 1!'),
+       (2, 'Note 3', 'Hello User1, Category 2!'),
+       (2, 'Note 4', 'Hello User1, Category 2!'),
+       (3, 'Note 1', 'Hello User2, Category 1!'),
+       (4, 'Note 2', 'Hello User2, Category 2!');
 
 INSERT INTO notes (category_id, title, content, deleted_flag, deleted_at)
 VALUES (1, 'Old Note 1', 'My old note. User1.', true, CURRENT_TIMESTAMP + (1 ||' minutes')::interval),
